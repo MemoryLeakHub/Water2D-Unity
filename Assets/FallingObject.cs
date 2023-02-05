95df3d2feed95e4c960627f6e59d3cce81f8ad04
Vladislav Rashkov <mrcrozer@gmail.com>
2023-02-05 17:07:49 +0200

    water

--- Assets/FallingObject.cs
+++ Assets/FallingObject.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class FallingObject : MonoBehaviour
 {
+    private Vector2 movementDirection;
     public Rigidbody2D rigidbody2D;
     [SerializeField]
     private float forceAmount;
+    private float timeElapsed;
+    private bool isMoving = false;
     void Start()
     {
         rigidbody2D.velocity = Vector3.down * forceAmount;
+    }
+
+    void Update() { 
+        
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        timeElapsed += Time.deltaTime;
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            isMoving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
+            isMoving = false;
+        }
+        if (isMoving) {
+            rigidbody2D.velocity = movementDirection * forceAmount;
+        }
     }
 }
